8f9330370260860a6d8b13795de46362abb79427
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-10-07 20:06:07 +0700

    Add files via upload

---
+++ src/kelipatan/Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kelipatan;
+
+/**
+ *
+ * @author 'KoalaX'
+ * NAMA     : Fiqri Akbar Pratama
+ * KELAS    : PBO1/IF1
+ * NIM      : 10118020
+ * Deksripsi Program : Program ini berisi program untuk menampilkan urutan 
+ * bilangan dengan kelipatan 3.5 hingga menyentuh angka 35.
+ */
+public class Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       for (double i = 3.5 ; i <= 35;i+= 3.5){
+           System.out.println(i+" "); 
+       }
+       System.out.println ("\nDevelop by : Fiqri Akbar Pratama");
+    }
+    
+}
